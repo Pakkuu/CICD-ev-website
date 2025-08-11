4e9eb663c9d0da3628063d5989fbf1e70e8ba349
Ethan Varghese <122655351+Pakkuu@users.noreply.github.com>
2025-08-11 15:48:16 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function AboutMe() {
         </div>
         <div className="flex relative">
           <div className="font-quicksand text-[#000000ac] font-[400] text-justify w-full mr-auto lg:text-[1.3rem] md:text-[1.1rem] sm:text-[1rem] text-[0.8rem] sm:leading-[3rem] leading-[2rem] pr-[1.5rem] z-10">
-            I&apos;m a sophmore studying computer science at UTD. In my free
+            I&apos;m a junior studying computer science at UTD. In my free
             time I love to workout, play video games, and watch anime (some of
             my favorites are Steins;Gate, FMAB, and Samurai Champloo). My interests
             within computer science lie in Cloud Services, RAG (Retrieval
